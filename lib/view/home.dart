fd7ec00ba422bcd2a738f374343ac9afbc012259
Aokiti <yuukiyuuki223@gmail.com>
2021-06-27 20:12:55 +0900

    検索バーのUI実装

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -35,38 +35,68 @@ class Home extends StatelessWidget {
 class SearchBar extends StatelessWidget with PreferredSizeWidget {
   @override
   Widget build(BuildContext context) {
+    final borderRadius = BorderRadius.all(Radius.circular(8));
     return Stack(
       children: <Widget>[
         Positioned(
-          top: 40,
-          right: 24,
-          left: 24,
+          top: 24,
+          right: 8,
+          left: 8,
           child: Container(
-            color: Colors.white,
-            child: Row(
-              children: <Widget>[
-                Material(
-                  type: MaterialType.transparency,
-                  child: IconButton(
-                    splashColor: Colors.grey,
-                    icon: Icon(Icons.menu),
-                    onPressed: () {
-                      Scaffold.of(context).openDrawer();
-                    },
-                  ),
-                ),
-                Expanded(
-                  child: TextField(
-                    cursorColor: Colors.black,
-                    keyboardType: TextInputType.text,
-                    textInputAction: TextInputAction.go,
-                    decoration: InputDecoration(
-                        border: InputBorder.none,
-                        contentPadding: EdgeInsets.symmetric(horizontal: 16),
-                        hintText: "原稿を検索"),
-                  ),
+            margin: const EdgeInsets.all(16.0),
+            decoration: BoxDecoration(
+              borderRadius: borderRadius,
+              color: Colors.white,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey[200],
+                  offset: Offset(0.0, 3.0),
+                  blurRadius: 16.0,
                 ),
               ],
+            ),
+            child: ClipRRect(
+              borderRadius: borderRadius,
+              child: Container(
+                margin: EdgeInsets.only(left: 4),
+                child: Row(
+                  children: <Widget>[
+                    ClipOval(
+                      child: Material(
+                        type: MaterialType.transparency,
+                        child: IconButton(
+                          splashColor: Colors.grey[50],
+                          icon: Icon(Icons.menu),
+                          onPressed: () {
+                            Scaffold.of(context).openDrawer();
+                          },
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Container(
+                        padding: EdgeInsets.only(left: 24),
+                        child: Text(
+                          "原稿を検索",
+                          style: TextStyle(
+                            color: Colors.black,
+                          ),
+                        ),
+                      ),
+                      // TextField(
+                      //   cursorColor: Colors.black,
+                      //   keyboardType: TextInputType.text,
+                      //   textInputAction: TextInputAction.go,
+                      //   decoration: InputDecoration(
+                      //       border: InputBorder.none,
+                      //       contentPadding:
+                      //           EdgeInsets.symmetric(horizontal: 16),
+                      //       hintText: "原稿を検索"),
+                      // ),
+                    ),
+                  ],
+                ),
+              ),
             ),
           ),
         ),
@@ -75,5 +105,5 @@ class SearchBar extends StatelessWidget with PreferredSizeWidget {
   }
 
   @override
-  Size get preferredSize => Size.fromHeight(64);
+  Size get preferredSize => Size.fromHeight(88);
 }
