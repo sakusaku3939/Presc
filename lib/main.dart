baf85d8ccc6af3500e4e49409f25c36773f072ad
sakusaku3939 <yuukiyuuki223@gmail.com>
2021-07-05 15:52:53 +0900

    フォントを日本語化

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:presc/view/screens/manuscript.dart';
 import 'package:provider/provider.dart';
 import 'package:presc/viewModel/bottom_navigation_bar_provider.dart';
@@ -30,6 +31,13 @@ class MyApp extends StatelessWidget {
           primaryColor: Colors.deepOrange[400],
           accentColor: Colors.deepOrange[400],
         ),
+        localizationsDelegates: [
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+        ],
+        supportedLocales: [
+          Locale('ja', ''),
+        ],
         home: ManuscriptScreen());
   }
 }
