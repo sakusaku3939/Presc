56e96b192b57cf66137d41673c7aebad0077f08e
Aokiti <yuukiyuuki223@gmail.com>
2021-07-05 13:46:33 +0900

    原稿カードの右上に三点リーダー追加

--- lib/view/utils/script_card.dart
+++ lib/view/utils/script_card.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_scale_tap/flutter_scale_tap.dart';
 import 'package:presc/view/screens/manuscript_edit.dart';
+import 'package:presc/view/utils/ripple_button.dart';
 
 Widget cardPageView() {
   final _itemList = ['one', 'two', 'three', 'for'];
@@ -69,29 +70,42 @@ class ScriptCard extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Container(
-              margin: const EdgeInsets.only(left: 16),
-              child: Align(
-                alignment: Alignment.topLeft,
-                child: Text(
-                  "原稿1",
-                  style: TextStyle(fontSize: 24),
-                ),
+              margin: const EdgeInsets.only(left: 12),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Expanded(
+                    child: Text(
+                      "原稿1",
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(fontSize: 24),
+                    ),
+                  ),
+                  RippleIconButton(
+                    child: IconButton(
+                      icon: Icon(Icons.more_vert),
+                      onPressed: () {},
+                    ),
+                  ),
+                ],
               ),
             ),
             DefaultTextStyle(
-                style: new TextStyle(color: Colors.black),
-                overflow: TextOverflow.ellipsis,
-                maxLines: 7,
-                child: new Padding(
-                  child: new Text(
-                    "吾輩は猫である。名前はまだ無い。どこで生れたかとんと見当がつかぬ。何でも薄暗いじめじめした所でニャーニャー泣いていた事だけは記憶している。吾輩はここで始めて人間というものを見た。しかもあとで聞くとそれは書生という人間中で一番獰悪な種族であったそうだ。この書生というのは時々我々を捕えて煮て食うという話である。しかしその当時は何という考もなかったから別段恐しいとも思わなかった。ただ彼の掌に載せられてスーと持ち上げられた時何だかフワフワした感じがあったばかりである。",
-                    style: TextStyle(
-                      color: Colors.grey[800],
-                      fontSize: 14,
-                    ),
+              style: TextStyle(color: Colors.black),
+              overflow: TextOverflow.ellipsis,
+              maxLines: 7,
+              child: Padding(
+                child: Text(
+                  "吾輩は猫である。名前はまだ無い。どこで生れたかとんと見当がつかぬ。何でも薄暗いじめじめした所でニャーニャー泣いていた事だけは記憶している。吾輩はここで始めて人間というものを見た。しかもあとで聞くとそれは書生という人間中で一番獰悪な種族であったそうだ。この書生というのは時々我々を捕えて煮て食うという話である。しかしその当時は何という考もなかったから別段恐しいとも思わなかった。ただ彼の掌に載せられてスーと持ち上げられた時何だかフワフワした感じがあったばかりである。",
+                  style: TextStyle(
+                    color: Colors.grey[800],
+                    fontSize: 14,
                   ),
-                  padding: const EdgeInsets.fromLTRB(12, 16, 4, 0),
-                )),
+                ),
+                padding: const EdgeInsets.fromLTRB(12, 16, 4, 0),
+              ),
+            ),
             Container(
               margin: const EdgeInsets.only(top: 16, right: 12),
               child: Align(
