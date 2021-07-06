d41314e34443b878e770f925d44d5a971c0a9ff7
sakusaku3939 <yuukiyuuki223@gmail.com>
2021-07-06 17:03:32 +0900

    編集画面のコンテンツ部分追加

--- lib/view/screens/manuscript_edit.dart
+++ lib/view/screens/manuscript_edit.dart
@@ -20,19 +20,20 @@ class ManuscriptEditScreen extends StatelessWidget {
               child: CustomScrollView(
                 slivers: <Widget>[
                   SliverAppBar(
-                      floating: true,
-                      snap: true,
-                      toolbarHeight: 80,
-                      expandedHeight: 80,
-                      backgroundColor: Colors.transparent,
-                      elevation: 0,
-                      leading: Container(),
-                      flexibleSpace: _menuBar(context)),
+                    floating: true,
+                    snap: true,
+                    pinned: true,
+                    toolbarHeight: 56,
+                    expandedHeight: 56,
+                    backgroundColor: Colors.white,
+                    elevation: 1,
+                    leading: Container(),
+                    flexibleSpace: _menuBar(context),
+                  ),
                   SliverList(
                     delegate: SliverChildListDelegate([_content()]),
-                  )
+                  ),
                 ],
-                // ),
               ),
             ),
           ),
@@ -100,7 +101,31 @@ class ManuscriptEditScreen extends StatelessWidget {
 
   Widget _content() {
     return Container(
-      child: Text("test"),
+      margin: EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            "原稿1",
+            overflow: TextOverflow.ellipsis,
+            style: TextStyle(fontSize: 24),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 16, bottom: 40),
+            child: Text(
+              "吾輩は猫である。名前はまだ無い。\n"
+              "どこで生れたかとんと見当がつかぬ。何でも薄暗いじめじめした所でニャーニャー泣いていた事だけは記憶している。吾輩はここで始めて人間というものを見た。しかもあとで聞くとそれは書生という人間中で一番獰悪な種族であったそうだ。この書生というのは時々我々を捕えて煮て食うという話である。しかしその当時は何という考もなかったから別段恐しいとも思わなかった。ただ彼の掌に載せられてスーと持ち上げられた時何だかフワフワした感じがあったばかりである。\n"
+              "\n"
+              "ようやくの思いで笹原を這い出すと向うに大きな池がある。吾輩は池の前に坐ってどうしたらよかろうと考えて見た。別にこれという分別も出ない。しばらくして泣いたら書生がまた迎に来てくれるかと考え付いた。ニャー、ニャーと試みにやって見たが誰も来ない。そのうち池の上をさらさらと風が渡って日が暮れかかる。腹が非常に減って来た。泣きたくても声が出ない。仕方がない、何でもよいから食物のある所まであるこうと決心をしてそろりそろりと池を左りに廻り始めた。どうも非常に苦しい。そこを我慢して無理やりに這って行くとようやくの事で何となく人間臭い所へ出た。",
+              style: TextStyle(
+                color: Colors.grey[800],
+                height: 1.7,
+                fontSize: 16,
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
