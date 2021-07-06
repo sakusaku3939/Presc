41aa9bdfe5616f9a22225f5c1cc3b555d55d9883
sakusaku3939 <yuukiyuuki223@gmail.com>
2021-07-06 16:14:12 +0900

    ボトムシートのUI完成

--- lib/view/utils/script_modal_bottom_sheet.dart
+++ lib/view/utils/script_modal_bottom_sheet.dart
@@ -4,6 +4,10 @@ class ScriptModalBottomSheet {
   void show(BuildContext context) {
     showModalBottomSheet(
       context: context,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(12),
+      ),
+      backgroundColor: Colors.white,
       builder: (ctx) {
         return _sheet(context);
       },
@@ -12,21 +16,37 @@ class ScriptModalBottomSheet {
 
   Widget _sheet(BuildContext context) {
     return Container(
-      padding: EdgeInsets.symmetric(horizontal: 24),
+      margin: EdgeInsets.only(bottom: 12),
       child: Column(
-      mainAxisSize: MainAxisSize.min,
-      children: <Widget>[
-        ListTile(
-          leading: Icon(Icons.play_arrow_outlined),
-          title: Text('原稿を再生'),
-          onTap: () => {},
-        ),
-        ListTile(
-          leading: Icon(Icons.delete_outline),
-          title: Text('ごみ箱に移動'),
-          onTap: () => {},
-        ),
-      ],
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Container(
+            width: 30,
+            height: 4,
+            margin: EdgeInsets.only(top: 6, bottom: 4),
+            decoration: BoxDecoration(
+              color: Colors.grey[300],
+              border: Border.all(color: Colors.grey[300]),
+              borderRadius: BorderRadius.circular(2),
+            ),
+          ),
+          ListTile(
+            leading: Transform.translate(
+              offset: Offset(8, 0),
+              child: Icon(Icons.play_arrow_outlined),
+            ),
+            title: Text('原稿を再生'),
+            onTap: () => {},
+          ),
+          ListTile(
+            leading: Transform.translate(
+              offset: Offset(8, 0),
+              child: Icon(Icons.delete_outline),
+            ),
+            title: Text('ごみ箱に移動'),
+            onTap: () => {},
+          ),
+        ],
       ),
     );
   }
