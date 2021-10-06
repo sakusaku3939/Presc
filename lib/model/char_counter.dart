4a2843319d7e171036aaefd6f937798810e06b3d
Aokiti <yuukiyuuki223@gmail.com>
2021-10-06 10:23:08 +0900

    メソッドを移動

--- lib/model/char_counter.dart
+++ lib/model/char_counter.dart
@@ -5,4 +5,7 @@ class CharCounter {
       text.replaceAll('\n', '').replaceAll(' ', '').length;
 
   static int lf(String text) => '\n'.allMatches(text).length;
+
+  static int countLine(String text) =>
+      text.split('\n').where((e) => e == '').length;
 }
