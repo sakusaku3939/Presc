b11c9a2c9bf70d28e3d08f372c5b3663a7598197
sakusaku3939 <yuukiyuuki223@gmail.com>
2021-10-19 15:54:02 +0900

    初期フォントサイズを20に変更

--- lib/config/init_config.dart
+++ lib/config/init_config.dart
@@ -4,6 +4,6 @@ class InitConfig {
   static const scrollMode = ScrollMode.recognition;
   static const scrollVertical = true;
   static const scrollSpeedMagnification = 1.0;
-  static const fontSize = 22;
+  static const fontSize = 20;
   static const fontHeight = 2.4;
 }
