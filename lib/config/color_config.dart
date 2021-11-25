0e554fafba465fa598fac9f5dc03d7df86363cb0
sakusaku3939 <yuukiyuuki223@gmail.com>
2021-11-25 21:31:10 +0900

    背景色を統一

--- lib/config/color_config.dart
+++ lib/config/color_config.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class ColorConfig {
-  static const backgroundColor = Color(0xFFF7F7F7);
+  static const backgroundColor = Color(0xFFFAFAFA);
   static const iconColor = Color(0xFF616161);
   static const playbackTextColor = Colors.white;
   static const playbackBackgroundColor = Color(0xFF212121);
